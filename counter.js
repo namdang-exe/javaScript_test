1244e7be86069d4ffd776a68be73eeac68a7214a
Dang <dangquangnam75@gmail.com>
2021-06-16 16:49:47 -0500

    currency

--- counter.js
+++ counter.js
@@ -1,13 +1,19 @@
-let counter = 0;
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 
 function count(){
+    let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h1').innerHTML = counter;
-
-    if (counter % 10 === 0){
-        alert(`The counter now is ${counter}`)
-    }
+    localStorage.setItem('counter', counter);
+    // if (counter % 10 === 0){
+    //     alert(`The counter now is ${counter}`)
+    // }
 }
 document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('button').addEventListener('click',count)
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+    document.querySelector('button').onclick = count;
+
+    // setInterval(count, 1000);
 });
